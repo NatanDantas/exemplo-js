9013d26191e2355a1f3ffc7e8988ed095e590045
NatanDantas <jsda201314@gmail.com>
2023-03-06 21:45:23 -0300

    Aula Spring Boot 0603

---
+++ src/test/java/br/senac/tads/dsw/exemplojs/ExemploJsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.tads.dsw.exemplojs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploJsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
